bd070b49265c79b6cff614c6252a13b70f32bff7
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-09-02 22:14:35 +0900

    Initial Commit

---
+++ LocaleInspector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocaleInspector
+//
+//  Created by Aaron Madlon-Kay on 9/2/16.
+//  Copyright © 2016 Aaron Madlon-Kay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
